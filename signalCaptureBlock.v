c90452c9b07a890a7e112c463beb416733d395d1
Vishwa <vish.eswaran@gmail.com>
2023-05-11 17:04:38 -0400

    SCLK triggering assymterically (not 50% duty cycle)

--- signalCaptureBlock.v
+++ signalCaptureBlock.v
@@ -1,50 +1,58 @@
 `timescale 1ns/1ps  
 
 module signalCaptureBlock(CLOCK_50, clkCounterEn); 
-
+  
+  parameter clockPeriodNS = 80; //needs to be a multiple of 20ns
+  parameter integer cycleCountMax = $ceil(clockPeriodNS/20); 
+  parameter clockCounterSize = $clog2(cycleCountMax); 
+  
 
   input CLOCK_50; 
   input clkCounterEn; 
- 
   
-  
+  wire [clockCounterSize - 1:0] sclkCounter; 
+	
   wire CLOCK_50; 
   wire clkCounterEn; 
   wire SCLK; 
   wire resetN; 
   
-  clockGenerator C1 (.fastClk(CLOCK_50), .SCLK(SCLK), .clkCounterEn(clkCounterEn), .resetN(resetN));
+  
+  clockGenerator #(.clockCounterSize(clockCounterSize)) C1 (.fastClk(CLOCK_50), .SCLK(SCLK), .clkCounterEn(clkCounterEn), .resetN(resetN), .sclkCounter(sclkCounter));
   
 endmodule 
 
 
+module clockGenerator #(parameter clockCounterSize = 2)(fastClk, SCLK, clkCounterEn, resetN, sclkCounter); 
 
+	//clock generator to generate 1/clockPeriodNS MHz from 50MHz	
 
-module clockGenerator #(parameter clockPeriodNS = 50)(fastClk, SCLK, clkCounterEn, resetN); 
+	parameter fullCycles = {clockCounterSize{1'b1}};
+	parameter fullCyclesBin = fullCycles - 1'b1; 
+	parameter halfCycles = $ceil(fullCycles/2); 
 
-	//clock generator to generate 1/clockPeriodNS MHz from 50MHz
-	//the clockPeriodNS needs to be a multiple of 10 for accuracy 
-
-   localparam clockCounterMax = clockPeriodNS/10 - 1;  
-	localparam clockCounterSize = $clog2(clockPeriodNS/10); 
-	localparam binCCS = {clockCounterSize{1'b1}}; 
-	
-	input fastClk;
+   input fastClk;
 	input clkCounterEn;
 	input resetN; 
-   output SCLK; 
-
-	reg [clockCounterSize -1:0] SCLK;    
-		
-	//assign fastClk_N = (clkCounterEn == 1'b1)?(~fastClk):1'b0; //inverted fastClock
 	
-	always @ (fastClk) begin 
+	output sclkCounter; 
+	output SCLK; 
+	
 
-	 if(!resetN | SCLK == binCCS)
-		SCLK <= 0; 
-	 else  
-	   SCLK <= SCLK + 1'b1;
-	 
+	wire SCLK; 
+   reg [clockCounterSize - 1:0] sclkCounter;    
+	
+	assign SCLK = (sclkCounter < halfCycles)? 1'b0 : 1'b1;  
+	
+	always @ (posedge fastClk) begin    
+		if(clkCounterEn == 1'b1) begin 	
+			 if(!resetN | sclkCounter == fullCyclesBin) 
+				sclkCounter <= 0; 
+			 else 
+				sclkCounter <= sclkCounter + 1'b1; 
+			end  
 	end 
+	
+	
 	
 endmodule 
--- wave.do
+++ wave.do
@@ -5,11 +5,16 @@ vsim signalCaptureBlock
 log {/*}
 add wave {/*}
 
-force {resetN} 1
+
+force {CLOCK_50} 0,1 10ns -r 20ns
+
+force {resetN} 0
 force {clkCounterEn} 0
-run 10ns
+run 25ns
 
 force {clkCounterEn} 1
-force {CLOCK_50} 0,1 10ns -r 20ns
-run 500ns  
+run 10ns
+
+force {resetN} 1
+run 1000ns
 
